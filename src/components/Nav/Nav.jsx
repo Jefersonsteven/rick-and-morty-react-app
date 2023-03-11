20bfba67c541349c66b8fa1f89a4ba1c5bfed6f2
Gfrey <gfreycode@gmail.com>
2023-03-11 15:16:49 -0500

    Termine el 80% de los estilo de login

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,17 +1,20 @@
 import './Nav.scss'
-import React from "react";
+import React, { useEffect } from "react";
 import { SearchBar } from '../SearchBar/SearchBar'
 import { Menu } from '../Menu/Menu'
+import { useLocation } from 'react-router-dom';
 
 function Nav() {
   
+  const location = useLocation();
+
   return(
     <nav className="Nav">
       <div className='Nav__menu'>
         <Menu />
       </div>
       <div className='Nav__search'>
-        <SearchBar/>
+        { location.pathname === '/home' && <SearchBar/> }
       </div>
     </nav>
   )
