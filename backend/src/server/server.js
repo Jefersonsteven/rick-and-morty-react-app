50f687fc6ebffa0dd95e13c29576ab48abdb05e1
Gfrey <gfreycode@gmail.com>
2023-03-28 09:04:54 -0500

    creando rama deploy

--- backend/src/server/server.js
+++ backend/src/server/server.js
@@ -3,30 +3,12 @@ const express = require('express');
 const cors = require('cors');
 const routerApi = require("../routes");
 const app = express();
-const port = 3001;
+
+const PORT =  process.env.PORT || 3001;
+console.log(`Server open in http://localhost:${PORT} 🚀`)
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(port);
+app.listen(PORT);
 routerApi(app);
-
-
-// const http = require("http");
-// const getChartById = require("../controllers/getCharById");
-// const getDetail = require("../controllers/getDetail");
-
-// const PORT = 3001;
-// console.log(`Server open in http://localhost:${PORT} 🚀`);
-
-// http
-//   .createServer((req, res) => {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     () => {
-//       const { url } = req;
-//       if (url.includes("onsearch")) getChartById(res, url.split("/").pop());
-//       if(url.includes("detail")) getDetail(res, url.split("/").pop())
-//     }
-
-//   })
-//   .listen(PORT, "localhost");
