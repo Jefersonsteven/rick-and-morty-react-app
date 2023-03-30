c25faeacd3c04ca45eae2650d449f8ee227d734c
Gfrey <gfreycode@gmail.com>
2023-03-30 11:09:23 -0500

    styles 95%

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -53,7 +53,7 @@ module.exports = {
   devServer: {
     static: path.join(__dirname, "build"),
     compress: true,
-    port: 3000,
+    port: 3005,
     open: true,
     historyApiFallback: true,
   },
