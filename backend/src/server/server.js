337d62279cc1118504df16a9ed51fc253055d912
Gfrey <gfreycode@gmail.com>
2023-03-30 07:12:24 -0500

    preparando el deploy a vercel

--- backend/src/server/server.js
+++ backend/src/server/server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT =  process.env.PORT || 3001;
 
 app.use(express.json());
-// app.use(cors());
+// app.use(cors()); 
 
 const whitelist = ['http://localhost:3000', 'http://myapp.co'];
 const options = {
