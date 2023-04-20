a4e5b24345facf57e2bdc8ea84d1552426dc6554
Gfrey <gfreycode@gmail.com>
2023-04-20 16:41:31 -0500

    deploy

--- backend/src/server/server.js
+++ backend/src/server/server.js
@@ -10,20 +10,20 @@ const PORT =  process.env.PORT || 3001;
 // {alter: true}
 conn.sync().then(() => {
     app.use(express.json());
-    const whitelist = [
-        "http://localhost:3000",
-        "https://rickandmorty-jeffer.netlify.app",
-    ];
-    const options = {
-        origin: (origin, callback) => {
-            if (whitelist.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback("No permitido", false);
-            }
-        },
-    };
-    app.use(cors(options));
+    // const whitelist = [
+    //     "http://localhost:3000",
+    //     "https://rickandmorty-jeffer.netlify.app",
+    // ];
+    // const options = {
+    //     origin: (origin, callback) => {
+    //         if (whitelist.includes(origin)) {
+    //             callback(null, true);
+    //         } else {
+    //             callback("No permitido", false);
+    //         }
+    //     },
+    // };
+    // app.use(cors(options));
     app.use(cors());
     app.listen(PORT);
     routerApi(app);
