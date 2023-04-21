76bb6350ced2c4b3f80369c6b35ce5d0cb32d84e
Gfrey <gfreycode@gmail.com>
2023-04-21 06:36:07 -0500

    deploy 3

--- backend/src/server/server.js
+++ backend/src/server/server.js
@@ -29,6 +29,7 @@ conn.sync().then(() => {
     app.use(cors());
     app.listen(PORT);
     routerApi(app);
+    res.setHeader("Access-Control-Allow-Origin", "*");
 });
 
 // Error catching endware.
